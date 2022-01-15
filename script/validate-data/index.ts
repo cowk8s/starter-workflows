3769a7d101364d69d0e21409991d64bb21bc51b6
Phan Tấn Tài <phantantai9800@gmail.com>
2022-01-15 20:22:43 +0700

    add package-lock for version 1

--- script/validate-data/index.ts
+++ script/validate-data/index.ts
@@ -1,4 +1,9 @@
-import { endGroup, error, info } from '@actions/core';
+#!/usr/bin/env npx ts-node
+import { promises as fs } from "fs";
+import { safeLoad } from "js-yaml";
+import { basename, extname, join } from "path";
+import { Validator as validator } from "jsonschema";
+import { endGroup, error, info, setFailed, startGroup } from '@actions/core';
 
 interface WorkflowWithErrors {
   id: string;
@@ -6,12 +11,64 @@ interface WorkflowWithErrors {
   errors: string[];
 }
 
+async function checkWorkflows(folders: string[]): Promise<WorkflowWithErrors[]> {
+  const result: WorkflowWithErrors[] = []
+  for (const folder of folders) {
+    const dir = await fs.readdir(folder, {
+      withFileTypes: true,
+    });
+
+    for (const e of dir) {
+      if (e.isFile() && [".yml", ".yaml"].includes(extname(e.name))) {
+        const fileType = basename(e.name, extname(e.name));
+
+        const workflowFilePath = join(folder, e.name);
+        const workflowWithErrors = await checkWorkflow(workflowFilePath);
+
+        if (workflowWithErrors.errors.length > 0) {
+          result.push(workflowWithErrors)
+        }
+      }
+    }
+  }
+  return result;
+}
+
+async function checkWorkflow(workflowPath: string): Promise<WorkflowWithErrors> {
+  let workflowErrors: WorkflowWithErrors = {
+    id: workflowPath,
+    name: null,
+    errors: []
+  }
+  try {
+    const workflowFileContent = await fs.readFile(workflowPath, 'utf8');
+    safeLoad(workflowFileContent);
+    workflowErrors.errors.push(`No icon named `)
+  } catch (e) {
+    workflowErrors.errors.push(e.toString())
+  }
+  return workflowErrors;
+}
+
 (async function main() {
   try {
-    info("🎉🤘 - Found no workflows with errors!")
+    const settings = require("./settings.json");
+    const erroredWorkflows = await checkWorkflows(
+      settings.folders
+    )
 
+    if (erroredWorkflows.length > 0) {
+      startGroup(`😟 - Found ${erroredWorkflows.length} workflows with errors:`);
+      erroredWorkflows.forEach(erroredWorkflow => {
+        error(`Errors in ${erroredWorkflow.id} - ${erroredWorkflow.errors.map(e => e.toString()).join(", ")}`)
+      })
+      endGroup();
+      setFailed(`Found ${erroredWorkflows.length} workflows with errors`);
+    } else {
+      info("🎉🤘 - Found no workflows with errors!")
+    }
   } catch (e) {
-    error(`Unhandled error while synching workflows: ${e.message}`);
-
+    error(`Unhandled error while syncing workflows: ${e}`);
+    setFailed(`Unhandled error`)
   }
 })();
